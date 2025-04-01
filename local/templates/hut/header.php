295916966b5447749d0b50d56040d6fde6079847
root <denisov@addamant.ru>
2025-04-01 07:13:37 +0300

    hut правка поиска

--- local/templates/hut/header.php
+++ local/templates/hut/header.php
@@ -283,40 +283,54 @@ $favoritesCount = $favorites->getFavoritesCount();
             </div>
         </div>
         <? $APPLICATION->IncludeComponent(
-            "arturgolubev:search.title",
-            "header",
-            array(
-                "ANIMATE_HINTS" => array(""),
-                "ANIMATE_HINTS_SPEED" => "1",
-                "CATEGORY_0" => array("iblock_hut"),
-                "CATEGORY_0_TITLE" => "",
-                "CATEGORY_0_iblock_hut" => IblockHelper::getIdByCode('hutMainCatalog'),
-                "CHECK_DATES" => "Y",
-                "CONTAINER_ID" => "smart-title-search",
-                "CONVERT_CURRENCY" => "N",
-                "FILTER_NAME" => "",
-                "INPUT_ID" => "smart-title-search-input",
-                "INPUT_PLACEHOLDER" => "Я ищу ...",
-                "NUM_CATEGORIES" => "1",
-                "ORDER" => "rank",
-                "PAGE" => "/catalog/",
-                "PREVIEW_HEIGHT_NEW" => "",
-                "PREVIEW_WIDTH_NEW" => "",
-                "PRICE_CODE" => array("BASE"),
-                "PRICE_VAT_INCLUDE" => "Y",
-                "SHOW_HISTORY" => "N",
-                "SHOW_HISTORY_POPUP" => "N",
-                "SHOW_INPUT" => "Y",
-                "SHOW_LOADING_ANIMATE" => "N",
-                "SHOW_PREVIEW" => "Y",
-                "SHOW_PREVIEW_TEXT" => "N",
-                "SHOW_PROPS" => array('TYPE', 'FULL_PREVIEW'),
-                "SHOW_QUANTITY" => "N",
-                "TOP_COUNT" => "5",
-                "USE_LANGUAGE_GUESS" => "Y",
-                "VOICE_INPUT" => "N"
-            )
-        ); ?>
+	"arturgolubev:search.title", 
+	"header", 
+	array(
+		"ANIMATE_HINTS" => array(
+		),
+		"ANIMATE_HINTS_SPEED" => "1",
+		"CATEGORY_0" => array(
+			0 => "iblock_1c_catalog",
+		),
+		"CATEGORY_0_TITLE" => "",
+		"CATEGORY_0_iblock_hut" => IblockHelper::getIdByCode("hutMainCatalog"),
+		"CHECK_DATES" => "Y",
+		"CONTAINER_ID" => "smart-title-search",
+		"CONVERT_CURRENCY" => "N",
+		"FILTER_NAME" => "",
+		"INPUT_ID" => "smart-title-search-input",
+		"INPUT_PLACEHOLDER" => "Я ищу ...",
+		"NUM_CATEGORIES" => "1",
+		"ORDER" => "rank",
+		"PAGE" => "/catalog/",
+		"PREVIEW_HEIGHT_NEW" => "",
+		"PREVIEW_WIDTH_NEW" => "",
+		"PRICE_CODE" => array(
+			0 => "BASE",
+		),
+		"PRICE_VAT_INCLUDE" => "Y",
+		"SHOW_HISTORY" => "N",
+		"SHOW_HISTORY_POPUP" => "N",
+		"SHOW_INPUT" => "Y",
+		"SHOW_LOADING_ANIMATE" => "N",
+		"SHOW_PREVIEW" => "Y",
+		"SHOW_PREVIEW_TEXT" => "N",
+		"SHOW_PROPS" => array(
+			0 => "TYPE",
+			1 => "FULL_PREVIEW",
+			2 => "",
+		),
+		"SHOW_QUANTITY" => "N",
+		"TOP_COUNT" => "5",
+		"USE_LANGUAGE_GUESS" => "Y",
+		"VOICE_INPUT" => "N",
+		"COMPONENT_TEMPLATE" => "header",
+		"CATEGORY_0_iblock_1c_catalog" => array(
+			0 => "107",
+		)
+	),
+	false
+); ?>
     </header>
     <main>
         <? $APPLICATION->ShowViewContent('catalog_banner'); ?>
