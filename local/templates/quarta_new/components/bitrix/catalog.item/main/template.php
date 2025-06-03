48341108ee477215852b9ea391bb4306b48f46f6
root <denisov@addamant.ru>
2025-06-03 12:31:10 +0300

    124800

--- local/templates/quarta_new/components/bitrix/catalog.item/main/template.php
+++ local/templates/quarta_new/components/bitrix/catalog.item/main/template.php
@@ -190,5 +190,26 @@ if (!empty($item['PREVIEW_PICTURE']) && is_array($item['PREVIEW_PICTURE'])) {
                 <?= GetMessage("BUY_ONE_CLICK") ?>
             </a>
         <? } ?>
+        <?php
+        if (isset($arParams['IS_FAVORITES']) && $arParams['IS_FAVORITES'] === 'Y') {
+            $APPLICATION->IncludeComponent(
+                "interlabs:oneclick",
+                "one-click",
+                Array(
+                    "AGREE_PROCESSING" => "N",
+                    "AJAX_MODE" => "Y",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "BUY_STRATEGY" => "OnlyProduct",
+                    "PRODUCT_ID" => $item['ID'],
+                    "USE_CAPTCHA" => "Y",
+                    "USE_FIELD_COMMENT" => "Y",
+                    "USE_FIELD_EMAIL" => "Y"
+                )
+            );
+        }
+        ?>
     </div>
 </div>
